9bc904bac0f1bc348a4bd6a070b277d7ea0ad24e
Mitrasree Deb <mhope@nimkicodes.local>
2024-11-02 01:03:23 -0700

    2024-nov-01-circular-sentence

--- 2490_circular-sentence.py
+++ 2490_circular-sentence.py
@@ -2,7 +2,7 @@ class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
         words = sentence.split()
         len_list = len(words)
-    
+        
         for i in range(len_list - 1):
             if words[i][-1] != words[i + 1][0]:
                 return False
