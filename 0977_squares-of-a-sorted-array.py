57a43bb0144729dab666308cdd375d316f01879a
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-16 23:53:17 -0800

    2024-nov-16-squares-of-a-sorted-array-O(n)

--- 0977_squares-of-a-sorted-array.py
+++ 0977_squares-of-a-sorted-array.py
@@ -1,11 +1,27 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
 
-        # bruteforce, O(n log n)
-        nums.sort(key=abs)
-        sorted_squares = []
+        # two pointers, O(n)
+        left, right = 0, len(nums)-1
+        index = len(nums)-1
+        sorted_squares = [0] * len(nums)
         
-        for i in range(len(nums)):
-            sorted_squares.append(nums[i]*nums[i])
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                sorted_squares[index] = pow(nums[left], 2)
+                left += 1
+            else:
+                sorted_squares[index] = pow(nums[right], 2)
+                right -= 1
+            index -= 1
         
         return sorted_squares
+
+        # bruteforce, O(n log n)
+        # nums.sort(key=abs)
+        # sorted_squares = []
+        
+        # for i in range(len(nums)):
+        #     sorted_squares.append(nums[i]*nums[i])
+        
+        # return sorted_squares
