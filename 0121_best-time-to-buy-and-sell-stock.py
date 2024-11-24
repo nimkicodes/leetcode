b4a7fdb093aa7cbde9fc32b03a19036d896ab735
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-24 00:50:28 -0800

    2024-nov-23-best-time-to-buy-and-sell-stock-kadanes-algo-O(n)

--- 0121_best-time-to-buy-and-sell-stock.py
+++ 0121_best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        # kadane's algorithm , O(n) time, O(1) space
+        profit = 0
+        buy_price = prices[0]
+
+        for current_price in prices[1:]:
+            if current_price < buy_price:
+                buy_price = current_price
+
+            profit = max(profit, current_price - buy_price)
+        
+        return profit
+
         # bruteforce, O(n2) time
         # constraint = (10^5)^2 -> 10^10 which is > 5x10^8, it gives TLE
 
-        profit = 0
+        # profit = 0
         
-        for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
-                if prices[j] - prices[i] > profit:
-                    profit = prices[j] - prices[i]
+        # for i in range(len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         if prices[j] - prices[i] > profit:
+        #             profit = prices[j] - prices[i]
 
-        if profit >= 0:
-            return profit
-        else:
-            return 0
+        # if profit >= 0:
+        #     return profit
+        # else:
+        #     return 0
