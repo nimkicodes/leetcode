a21a99b4fbb57dd482adeeeb908028d341df58bc
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-27 02:02:00 -0800

    2024-nov-27-reverse-string-but-more-labour

--- 0344_reverse-string.py
+++ 0344_reverse-string.py
@@ -1,3 +1,13 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        # O(n) time, O(1) space
+        # s.reverse()
+
+        # O(n) time, O(1) space 
+        # just more labour, that's it
+        start, end = 0, len(s)-1
+
+        while start < end:
+            s[start] , s[end] = s[end], s[start]
+            start += 1
+            end -= 1
