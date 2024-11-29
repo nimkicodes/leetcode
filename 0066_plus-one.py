78f453f01838267646ea3e53054f8af6ed0fc825
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-28 17:23:04 -0800

    2024-nov-26-plus-one-modified

--- 0066_plus-one.py
+++ 0066_plus-one.py
@@ -2,6 +2,6 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         num = "".join((map(str, digits)))
         new_num = int(num) + 1
-        plus_one = list(map(lambda x: int(x), str(new_num)))
+        plus_one = list(map(int, str(new_num)))
         
         return plus_one
