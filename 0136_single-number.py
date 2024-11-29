6436ba17ab1be025810b91692f5e5c2b2666ccbd
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-28 17:19:20 -0800

    2024-nov-27-single-number-other-approaches

--- 0136_single-number.py
+++ 0136_single-number.py
@@ -4,10 +4,27 @@ class Solution:
         # xor works bitwise and commutative, A xor A = 0, A xor 0 = A
         xor = nums[0]
         
-        for i in range(1, len(nums)):
-            xor = xor ^ nums[i]
+        for x in nums[1:]:
+            xor = xor ^ x
         
         return xor 
+
+        # O(n), O(n)
+        # dictionary 
+
+        # counter = defaultdict(int)
+        # for x in nums:
+        #     counter[x] += 1
+
+        # for k,v in counter.items():
+        #     if v == 1:
+        #         return k
+        
+        # O(nlogn), O(1)
+        # nums.sort()
+        # for i in range(0, len(nums), 2):
+        #     if i+1 >= len(nums) or nums[i] != nums[i+1]:
+        #         return nums[i]
         
         # O(n2), O(1)
         # for n in nums:
