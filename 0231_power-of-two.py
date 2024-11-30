86c340f29ab1cbf50b347429e83c25c781c0d65e
Mitrasree Deb <mitrasree091@gmail.com>
2024-11-30 01:46:22 -0800

    2024-nov-30-power-of-two-bitwise-manipulation

--- 0231_power-of-two.py
+++ 0231_power-of-two.py
@@ -1,11 +1,18 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 1:
+        # O(1), O(1)
+        if n>0 and n & (n-1) == 0:
             return True
-
-        elif n%2 == 0:
-            for i in range(31):
-                if pow(2,i) == n:
-                    return True
+        else:
+            return False
         
-        return False
+        # O(logn), O(1)
+        # if n == 1:
+        #     return True
+
+        # elif n%2 == 0:
+        #     for i in range(31):
+        #         if pow(2,i) == n:
+        #             return True
+        
+        # return False
