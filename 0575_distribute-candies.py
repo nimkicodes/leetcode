0d5b501afd887a4b3d77e6a2c60dab650d1c8aab
Mitrasree Deb <mitrasree091@gmail.com>
2024-12-18 23:17:41 +0100

    2024-dec-18-distribute-candies-with-set

--- 0575_distribute-candies.py
+++ 0575_distribute-candies.py
@@ -1,8 +1,9 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        candy_type = defaultdict(int)
+        return min(len(candyType)//2, len(set(candyType)))
+        # candy_type = defaultdict(int)
         
-        for variety in candyType:
-            candy_type[variety] += 1
+        # for variety in candyType:
+        #     candy_type[variety] += 1
         
-        return min(len(candyType)//2, len(candy_type))
+        # return min(len(candyType)//2, len(candy_type))
