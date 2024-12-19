3c6830d3cc688b0a081f5b223e0e838c55ca08fe
Mitrasree Deb <mitrasree091@gmail.com>
2024-12-19 19:34:35 +0100

    2024-dec-19-minimum-index-sum-of-two-lists-optimized

--- 0599_minimum-index-sum-of-two-lists.py
+++ 0599_minimum-index-sum-of-two-lists.py
@@ -1,27 +1,17 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        common_string = list(set(list1) & set(list2))
-        
-        if len(common_string) == 1:
-            return common_string
-
         dict1 = {}
-        for i in range(len(list1)):
-            dict1[list1[i]] = i
-
-        dict2 = {}
-        for i in range(len(list2)):
-            dict2[list2[i]] = i
+        for index, item in enumerate(list1):
+            dict1[item] = index
         
         result = []
         minimum = float(inf)
-        for item in common_string:
-            if item in dict1 and item in dict2:
-                index_sum = dict1.get(item) + dict2.get(item)
+        for index, item in enumerate(list2):
+            if item in dict1:
+                index_sum = index + dict1[item]
                 if index_sum < minimum: 
                     result = [item]
                     minimum = index_sum
                 elif index_sum == minimum:
                     result.append(item)
-        
         return result
