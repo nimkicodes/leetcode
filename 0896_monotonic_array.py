e1e7c0155ec9b26cf1eeb6bc6888dab5fe19b972
Mitrasree Deb <mitrasree091@gmail.com>
2025-01-12 23:40:46 -0800

    2025-jan-12-monotonic-array

---
+++ 0896_monotonic_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        # increasing, decreasing = True, True
+        # for i in range(1, len(nums)):
+        #     if nums[i] < nums[i-1]:
+        #         increasing = False 
+            
+        #     if nums[i] > nums[i-1]:
+        #         decreasing = False
+        
+        # return increasing or decreasing
+        
+        increasing = all(x <= y for x, y in zip(nums, nums[1:]))
+        decreasing = all(x >= y for x, y in zip(nums, nums[1:]))
+        
+        return increasing or decreasing
