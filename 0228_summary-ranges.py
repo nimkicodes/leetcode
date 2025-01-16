06af6b032c7db9444a20d0bc408bcf86e8efeb52
Mitrasree Deb <mitrasree091@gmail.com>
2025-01-15 23:58:15 -0800

    2025-jan-15-summary-ranges-lifeishectic

--- 0228_summary-ranges.py
+++ 0228_summary-ranges.py
@@ -7,7 +7,7 @@ class Solution:
         i = 0
         while i < len(nums):
             start = nums[i] 
-            while i+1 < len(nums) and nums[i] + 1 == nums[i+1]:
+            while i + 1 < len(nums) and nums[i] + 1 == nums[i+1]:
                 i += 1
             
             if start == nums[i]:
