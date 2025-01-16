2ba8ed9f2884f3298f01f634b7b89951bcf93a9c
Mitrasree Deb <mitrasree091@gmail.com>
2025-01-15 23:57:03 -0800

    2025-jan-15-summary-ranges

---
+++ 0228_summary-ranges.py
@@ -0,0 +1,20 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if nums == []:
+            return []
+
+        result = []
+        i = 0
+        while i < len(nums):
+            start = nums[i] 
+            while i+1 < len(nums) and nums[i] + 1 == nums[i+1]:
+                i += 1
+            
+            if start == nums[i]:
+                result.append(f"{start}")
+            else:
+                result.append(f"{start}->{nums[i]}")
+            
+            i += 1
+        
+        return result
