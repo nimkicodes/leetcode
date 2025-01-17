afeb3deaef0116a13aa7cf8df8e90353af0a21ba
Mitrasree Deb <mitrasree091@gmail.com>
2025-01-16 17:21:58 -0800

    2025-jan-17-number-of-distinct-averages

---
+++ 2465_number-of-distinct-averages.py
@@ -0,0 +1,12 @@
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        nums.sort()
+        averages = set()
+
+        i, j = 0, len(nums)-1
+        while i < j: 
+            averages.add( (nums[i] + nums[j]) / 2)
+            i += 1
+            j -= 1
+
+        return len(averages)          
