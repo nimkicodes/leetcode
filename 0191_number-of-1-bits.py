5fafab204b7a611948c0d01de1567c631ab77a13
Mitrasree Deb <mitrasree091@gmail.com>
2025-03-02 23:57:21 -0800

    2025-mar-02-number-of-1-bits

---
+++ 0191_number-of-1-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            if (n >> i) & 1:
+                result += 1
+
+        return result
