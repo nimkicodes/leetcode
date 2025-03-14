165c674265a715192af08c60c59955d735f1e476
Mitrasree Deb <mitrasree091@gmail.com>
2025-03-14 00:22:44 -0700

    2025-mar-13-calculate-digit-sum-of-a-string

--- 2243_calculate-digit-sum-of-a-string.py
+++ 2243_calculate-digit-sum-of-a-string.py
@@ -1,17 +1,11 @@
 class Solution:
     def digitSum(self, s: str, k: int) -> str:
         def divideString(s: str, k: int) -> List[str]: 
-            l, n = [], len(s)
-            for i in range(0, n, k):
-                l.append(s[i:min(i + k, n)])
-            return l
-        
-        while len(s)>k: 
-            arr, temp = divideString(s, k), [] 
-            for group in arr: 
-                group_sum = 0
-                for digit in group:
-                    group_sum += int(digit)
-                temp.append(str(group_sum)) 
-            s = ''.join(temp) 
+            return [s[i:i+k] for i in range(0, len(s), k)] 
+
+        while k < len(s):  
+            groups = divideString(s, k)
+            group_sum = [str(sum(int(digit) for digit in group)) for group in groups]  
+            s = "".join(group_sum) 
+
         return s
