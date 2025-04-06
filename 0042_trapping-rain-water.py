70f7e63e68cbec46cc410ba655957fca272a1863
Mitrasree Deb <mitrasree091@gmail.com>
2025-04-05 23:34:24 -0700

    2025-apr-04-trapping-rain-water-updated

--- 0042_trapping-rain-water.py
+++ 0042_trapping-rain-water.py
@@ -1,23 +1,40 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # two pointers, O(n)
-        left , right = 0, len(height)-1
-        max_left, max_right = height[left], height[right]
-        water = 0
+        # O(1) time, O(n) space
+        n, water = len(height), 0
+        left, right = [0]*n , [0]*n
+        left[0], right[n-1] = height[0], height[n-1]
 
-        while left < right:
-            if max_left < max_right:
-                left += 1
-                max_left = max(max_left, height[left])
-                water += max(0, max_left - height[left])
-            else:
-                right -= 1
-                max_right = max(max_right, height[right])
-                water += max(0, max_right - height[right])
+        for i in range(1, n):
+            left[i] = max(left[i-1], height[i])
+        
+        for i in range(n-2, 0, -1):
+            right[i] = max(right[i+1], height[i])
+
+        for i in range(n):
+            h = min(left[i], right[i])     
+            water += max(0, h - height[i])
 
         return water
         
-        # bruteforce, O(n2)
+        # two pointers, O(n) time, O(1) space
+        # left , right = 0, len(height)-1
+        # max_left, max_right = height[left], height[right]
+        # water = 0
+
+        # while left < right:
+        #     if max_left < max_right:
+        #         left += 1
+        #         max_left = max(max_left, height[left])
+        #         water += max(0, max_left - height[left])
+        #     else:
+        #         right -= 1
+        #         max_right = max(max_right, height[right])
+        #         water += max(0, max_right - height[right])
+
+        # return water
+        
+        # bruteforce, O(n2) time, space O(1)
         # water = 0
         
         # for i in range(1, len(height)-1):
