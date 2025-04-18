00341bf411587d8412a2757b4521dfc6905ddf48
Mitrasree Deb <mitrasree091@gmail.com>
2025-04-17 23:20:02 -0700

    updated-coin-change-DP

--- 0322_coin-change.py
+++ 0322_coin-change.py
@@ -1,20 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}
+        memo = [None]* (amount+1)
         
-        def rec(coins, amount):
-            if amount == 0:
+        def dp(n):
+            if n == 0:
                 return 0
 
-            if amount in dp:
-                return dp[amount]
+            if memo[n] is not None:
+                return memo[n]
         
             minimum = inf
             for coin in coins:
-                if (amount - coin) >= 0:
-                    minimum = min(minimum, rec(coins, amount - coin) + 1)
+                if (n - coin) >= 0:
+                    minimum = min(minimum, dp(n - coin) + 1)
                 
-            dp[amount] = minimum
-            return dp[amount]
+            memo[n] = minimum
+            return memo[n]
 
-        return rec(coins, amount) if rec(coins, amount) != inf else -1
+        return dp(amount) if dp(amount) != inf else -1
