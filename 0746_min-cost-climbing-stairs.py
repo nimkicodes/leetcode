fafe09515eeb9d74f1bbd3c45ff51b8e3cf63f9e
Mitrasree Deb <mitrasree091@gmail.com>
2025-04-17 22:42:44 -0700

    updated-min-cost-climbing-stairs-DP

--- 0746_min-cost-climbing-stairs.py
+++ 0746_min-cost-climbing-stairs.py
@@ -1,8 +1,29 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
+        # bottom-up
+        n = len(cost)
+        dp = [0] * n
+        dp[0], dp[1] = cost[0], cost[1]
         
-        for i in range(len(cost)-4, -1, -1):
-            cost[i] += min(cost[i+1], cost[i+2])
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
         
-        return min(cost[0], cost[1])
+        return min(dp[n - 1], dp[n - 2])
+        
+        # top-down 
+        # memo = [None] * len(cost)
+        # def dp(n):
+        #     if n < 2:
+        #         return cost[n]
+        #     if memo[n] is not None:
+        #         return memo[n]
+        #     memo[n] = cost[n] + min(dp(n-1), dp(n-2))
+        #     return memo[n]
+
+        # return min(dp(len(cost)-1), dp(len(cost)-2))
+        
+        # cost.append(0)
+        # for i in range(len(cost)-4, -1, -1):
+        #     cost[i] += min(cost[i+1], cost[i+2])
+        
+        # return min(cost[0], cost[1])
