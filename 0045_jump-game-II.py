3eafd26494cd06deaa3d89d7add80dfa92110eb4
Mitrasree Deb <mitrasree091@gmail.com>
2025-04-24 04:47:13 -0700

    2025-apr-23

--- 0045_jump-game-II.py
+++ 0045_jump-game-II.py
@@ -1,13 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         jumps = 0
-        current, farthest = 0, 0
-        
+        window_end, farthest = 0, 0
+        best_index = 0
+
         for i in range(len(nums) - 1):  
-            farthest = max(farthest, i + nums[i])
-            
-            if i == current:  
+            if i + nums[i] > farthest:
+                farthest = i + nums[i]
+                best_index = i
+
+            if i == window_end:
                 jumps += 1
-                current = farthest
-                
-        return jumps  
+                print("jump"+ str(jumps) + " : jump from " + str(best_index))
+                window_end = farthest
+        
+        return jumps
