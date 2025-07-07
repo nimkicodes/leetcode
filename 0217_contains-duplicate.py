54441e5b3862d906842933ad93addb1b2edd6117
Mitrasree Deb <mitrasree091@gmail.com>
2025-07-06 23:18:08 -0700

    2025-jul-05-contains-duplicates

---
+++ 0217_contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+        # count = Counter(nums)
+        # for count in count.values():
+        #     if count > 1:
+        #         return True
+        # return False
