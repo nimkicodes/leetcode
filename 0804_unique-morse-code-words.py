767ae57a8ee83acf83f718c40cbcae224fcfba11
Mitrasree Deb <mitrasree091@gmail.com>
2025-07-23 00:29:54 -0700

    2025-jul-22-unique-morse-code-words

---
+++ 0804_unique-morse-code-words.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        d = {'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".", 'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---", 'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---", 'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-", 'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--.."}
+
+        result = set()
+        for word in words:
+            current = ""
+            for char in word:
+                current += d[char]
+            result.add(current)
+        
+        return len(result)
